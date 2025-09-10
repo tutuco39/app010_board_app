3ae2da375e40fdeb6b7345ed3ff9c345859eb30d
つづこ <>
2025-09-11 01:21:56 +0900

    アバター変更・保存・更新、ダミーユーザーのアバター設定

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,28 +8,52 @@
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
 
-# 10.times do
-#   User.create!(
+# # 自分
+# user = User.create!(
+#   email: "master@email.com",
+#   password: "aa1234"
+# )
+
+# # avatarなしのユーザー
+# 2.times do |i|
+#   user = User.create!(
 #     email: Faker::Internet.email,
-#     password: "aa1234",
-#     encrypted_password: "aa1234"
-#     )
+#     password: "aa1234"
+#   )
 # end
 
-users = User.all
+# 10.times do |i|
+#   user = User.create!(
+#     email: Faker::Internet.email,
+#     password: "aa1234"
+#   )
 
-20.times do
+#   # 画像を添付（例: samples/avatars/avatar1.png）
+#   avatar_path = Rails.root.join("db/samples/avatars/avatar#{i % 10 + 1}.png")
+#   user.avatar.attach(
+#     io: File.open(avatar_path),
+#     filename: "avatar#{i + 1}.png",
+#     content_type: "image/png"
+#   )
+# end
+
+users = User.order(:id).to_a
+user_cycle = users.cycle   # ←順番に取り出せる
+
+5.times do
   topic = Topic.create!(
-    title: Faker::Lorem.sentence(word_count: 10),
-    body: Faker::Lorem.sentence(word_count: 100),
+    title: Faker::Lorem.sentence(word_count: 5),
+    body:  Faker::Lorem.sentence(word_count: 100),
     user: users.sample
+    # user:  user_cycle.next          # ←トピック作成者は順番に
   )
 
-  # 2〜3件のコメントをランダムで作成
+  # コメントも順番に（件数はランダム）
   rand(10..15).times do
     topic.comments.create!(
       body: Faker::Lorem.sentence(word_count: 50),
       user: users.sample
+      # user: user_cycle.next          # ←コメント投稿者も順番に
     )
   end
 end
